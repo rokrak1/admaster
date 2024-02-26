eed6dfdaf8ac4adbb86da4f637cde9d59bedbc7b
rok <rok.rak1@gmail.com>
2024-02-26 23:34:35 +0100

    concurrent image downloading, saving images to cdn

--- server/app/functions/storage.py
+++ server/app/functions/storage.py
@@ -2,6 +2,10 @@ import pandas as pd
 import hashlib
 from io import BytesIO
 import io
+import numpy as np
+import concurrent.futures
+import requests
+import os
 
 def generate_hash(row):
     # Create a string representation of the row
@@ -15,7 +19,7 @@ def hash_csv(csv_file):
     try:
         df = pd.read_csv(csv_file, sep=';', quotechar='"')
     except pd.errors.ParserError as e:
-        print(f"An error occurred: {e}")
+        print(f"Hashing csv error: {e}")
 
     # Apply the function to each row to create a new 'hash' column
     df['hash'] = df.apply(generate_hash, axis=1)
@@ -83,24 +87,104 @@ def check_csv_exists(ftp):
     return df
 
 def compare_data_frames_and_return_new(new_df, existing_df):
-    # Merge the two DataFrames on 'id' to find common rows
-    merged_df = pd.merge(new_df, existing_df, on='id', suffixes=('_new', '_existing'), how='outer', indicator=True)
+    # Merge the two DataFrames on 'id' to find common rows, indicating the source of each row
+    merged_df = pd.merge(new_df, existing_df, on='id', suffixes=('_new', '_old'), how='outer', indicator=True)
+
+    original_columns = [col for col in new_df.columns if col != 'id']
+
+    # New rows
+    new_rows = merged_df[merged_df['_merge'] == 'left_only']
+    new_rows_cleaned = new_rows[['id'] + [f'{col}_new' for col in original_columns]]
+    new_rows_cleaned.columns = ['id'] + original_columns
     
-    # Identify rows that are either updated or only exist in new_df
-    updated_or_new = (merged_df['hash_new'] != merged_df['hash_existing']) | (merged_df['_merge'] == 'left_only')
-    
-    # TODO: fix id error 
-    # For rows that are updated or new, take data from new_df
-    merged_df.loc[updated_or_new, existing_df.columns] = merged_df.loc[updated_or_new, [col + '_new' for col in existing_df.columns]].values
-    
-    # Recompute hashes for updated or new rows
-    columns_for_hashing = [col for col in new_df.columns if col != 'hash_new']  # Exclude the 'hash' column
-    merged_df.loc[updated_or_new, 'hash_new'] = merged_df.loc[updated_or_new, columns_for_hashing].apply(generate_hash, axis=1)
-    
-    # Drop rows that only exist in existing_df and not in new_df
-    merged_df = merged_df[merged_df['_merge'] != 'right_only']
-    
-    # Drop temporary columns and rename columns back
-    merged_df.drop(columns=[col for col in merged_df.columns if '_new' in col or '_existing' in col or col == '_merge'], inplace=True)
-    
-    return merged_df
+    # Removed rows
+    removed_rows = merged_df[merged_df['_merge'] == 'right_only']
+    removed_rows_cleaned = removed_rows[['id'] + [f'{col}_old' for col in original_columns]]
+    removed_rows_cleaned.columns = ['id'] + original_columns
+
+    # Updated row
+    updated_rows = merged_df[(merged_df['_merge'] == 'both') & (merged_df['hash_new'] != merged_df['hash_old'])]
+    updated_rows_cleaned = updated_rows[['id'] + [f'{col}_new' for col in original_columns]]
+    updated_rows_cleaned.columns = ['id'] + original_columns
+
+    # Rows with new image URLs
+    updated_rows_new_urls = updated_rows[updated_rows['image_link_new'] != updated_rows['image_link_old']]
+    updated_rows_new_urls_cleaned = updated_rows_new_urls[['id'] + [f'{col}_new' for col in original_columns if col != 'image_link'] + ['image_link_new']]
+    updated_rows_new_urls_cleaned.columns = ['id'] + [col for col in original_columns if col != 'image_link'] + ['image_link']
+
+
+    updated_rows = {
+        "new": new_rows_cleaned,
+        "removed": removed_rows_cleaned,
+        "updated": updated_rows_cleaned,
+        "updated_new_urls": updated_rows_new_urls_cleaned
+    }
+    return new_df, updated_rows
+
+def fetch_image_content(row: pd.Series):
+    image_url = row['image_link']
+    try:
+        response = requests.get(image_url, stream=True, timeout=(10, 30))
+        if response.status_code == 200:
+            # TODO REMOVE BACKGROUND FROM IMAGE IF WHITE
+            row['image_content'] = response.content
+            return row, True
+        else:
+            print(f"Failed to download {image_url}: Status code {response.status_code}")
+            return None, False
+    except Exception as e:
+        print(f"Failed to download {image_url}: {e}")
+        raise ValueError(f"Error downloading {image_url}: {e}")
+
+def batch_fetch_images(rows, n_jobs=8):
+    """Yield image content as images are fetched."""
+    with concurrent.futures.ThreadPoolExecutor(max_workers=n_jobs) as executor:
+        # Submit tasks to fetch image content
+        print("oask1")
+        future_to_url = {executor.submit(fetch_image_content, row): row for index, row in rows.iterrows()}
+
+        
+        # As tasks complete, yield the image content
+        for future in concurrent.futures.as_completed(future_to_url):
+            print("oask2")
+            response = future_to_url[future]
+            try:
+                new_row, is_valid = future.result()
+                if is_valid:
+                    yield new_row, True
+                else:
+                    print(f"Failed to fetch image from {response['image_link']}")
+            except Exception as exc:
+                print(f"{response['image_link']} generated an exception: {exc}")
+
+def batch_download_images_to_cdn(rows, ftp):
+    for new_row, is_valid in batch_fetch_images(rows, n_jobs=8):
+        if is_valid:
+            save_to_storage(new_row, ftp)
+
+def save_to_storage(row: pd.Series, ftp):
+    image_content = row['image_content']
+    image_id = row['id'].replace("/", "_")
+    image_url = row['image_link']
+    image_extension = os.path.splitext(image_url)[1] 
+    filename = f"{image_id}{image_extension}"
+    print(f"Saving image {filename} to storage.")
+    try:
+        # TODO ADD USER FOLDER BASED ON ID AND DATA_FOLDER
+        ftp.storbinary(f'STOR {filename}', BytesIO(image_content))
+    except Exception as e:
+        print(f"Save to storage error: {e}")
+
+def remove_cached_images(removed_rows, ftp):
+    """Remove images from the FTP server for removed rows."""
+    # Iterate over the rows and remove the images from the FTP server.
+    for index, row in removed_rows.iterrows():
+        image_id = row['id'].replace("/", "_")
+        image_url = row['image_link']
+        image_extension = os.path.splitext(image_url)[1] 
+        filename = f"{image_id}{image_extension}"
+        try:
+            # TODO ADD USER FOLDER BASED ON ID AND DATA FOLDER
+            ftp.delete(filename)
+        except Exception as e:
+            print(f"Save to storage error: {e}")
--- server/app/routes/database.py
+++ server/app/routes/database.py
@@ -1,6 +1,7 @@
 from fastapi import File, UploadFile, Request
+from fastapi.responses import JSONResponse
 from app.routers import protected_router, public_router
-from app.functions.storage import hash_csv, save_csv_to_storage, check_csv_exists, compare_data_frames_and_return_new
+from app.functions.storage import hash_csv, save_csv_to_storage, check_csv_exists, compare_data_frames_and_return_new, batch_download_images_to_cdn
 from app.config.ftp import create_ftp_connection
 from io import StringIO
 
@@ -18,7 +19,7 @@ async def upload_csv(csv_file: UploadFile = File(...)):
     with create_ftp_connection() as ftp:
         if ftp is None:
             print("FTP connection is not established.")
-            return {"message": "Failed to connect to the FTP server."}
+            return JSONResponse(content={"message": "Failed to connect to the FTP server."})
 
         # Hash the CSV file
         csv_data = None
@@ -26,24 +27,41 @@ async def upload_csv(csv_file: UploadFile = File(...)):
             content = csv_file.file.read().decode('utf-8')
             csv_data = StringIO(content)
         except Exception as e:
-            print(f"An error occurred: {e}")
+            print(f"Reading csv error: {e}")
+            JSONResponse(content={"message": "Failed to process the CSV file."})
 
         if not csv_data:
-            return {"message": "Failed to process the CSV file."}
+            return JSONResponse(content={"message": "Failed to process the CSV file."})
 
         hashed_csv = hash_csv(csv_data)
-
         # Check if CSV exists and compare data
         csv_from_storage = check_csv_exists(ftp)
 
+        updated_rows = None
         if csv_from_storage is not None:
-            hashed_csv = compare_data_frames_and_return_new(hashed_csv, csv_from_storage)
-
+            try:
+                hashed_csv, updated_rows = compare_data_frames_and_return_new(hashed_csv, csv_from_storage)
+            except Exception as e:
+                print(f"Compare dataframes error: {e}")
+                return JSONResponse(content={"message": "Failed to compare the CSV data."})
+        
+        print("err1")
+        # Remove old images or create new ones
+        if updated_rows is not None:
+            if updated_rows["removed"]:
+                remove_cached_images(updated_rows["removed"], ftp)
+            if updated_rows["new"]:
+                batch_download_images_to_cdn(updated_rows["new"], ftp)
+            if updated_rows["updated_new_urls"]:
+                batch_download_images_to_cdn(updated_rows["updated_new_urls"], ftp)
+        else:
+            batch_download_images_to_cdn(hashed_csv, ftp)
+       
         # Save the CSV file to the FTP server
         try:
             save_csv_to_storage(hashed_csv, ftp, "hashed_data_feed.csv")
         except Exception as e:
-            print(f"An error occurred: {e}")
-            return {"message": "Failed to save the CSV file to the FTP server."}
+            print(f"Save csv to storage error: {e}")
+            return JSONResponse(content={"message": "Failed to save the CSV file to the FTP server."})
 
-    return {"message": "CSV file processed successfully"}
+    return JSONResponse(content={"message": "CSV file processed successfully"})
